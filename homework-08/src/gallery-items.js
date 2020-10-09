d4fac365409904c225f1b8e105fa2604a8da4a24
OleksandrGrubyi <ghrubyi@ukr.net>
2020-10-10 00:58:27 +0300

    add all except button right, left

--- homework-08/src/gallery-items.js
+++ homework-08/src/gallery-items.js
@@ -1,4 +1,4 @@
-export default [
+const pictures = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,3 +63,80 @@ export default [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+{
+  /* <div class="lightbox js-lightbox">
+  <div class="lightbox__overlay"></div>
+
+  <div class="lightbox__content">
+    <img class="lightbox__image" src="" alt="" />
+  </div> */
+}
+
+const ref = {
+  galary: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.js-lightbox'),
+  closeBtn: document.querySelector('.lightbox__button'),
+  lightbox: document.querySelector('.lightbox__content'),
+  isOpen: document.querySelector('.is-open'),
+  imgOnModal: document.querySelector('.lightbox__image'),
+  btnNextModal: document.querySelector('.modalbtn-next'),
+  btnPreviousModal: document.querySelector('.modalbtn-prev'),
+  overlay: document.querySelector('.lightbox__overlay'),
+};
+
+ref.galary.addEventListener('click', onClickOpenModal);
+ref.closeBtn.addEventListener('click', onClickCloseModal);
+ref.overlay.addEventListener('click', onClickOverlayCloseModal);
+
+function makePictureMurkup(picture) {
+  return picture
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+    })
+    .join('');
+}
+const allPicture = makePictureMurkup(pictures);
+ref.galary.insertAdjacentHTML('beforeend', allPicture);
+
+function onClickOpenModal(event) {
+  event.preventDefault();
+  const originalPicture = event.target.dataset.source;
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  window.addEventListener('keydown', onEscapeCloseModal);
+  ref.modal.classList.add('is-open');
+  ref.imgOnModal.src = originalPicture;
+}
+
+function onClickCloseModal(event) {
+  ref.modal.classList.remove('is-open');
+  ref.imgOnModal.src = '';
+  window.removeEventListener('keydown', onEscapeCloseModal);
+}
+
+function onClickOverlayCloseModal(event) {
+  if (event.target === event.currentTarget) {
+    onClickCloseModal();
+  }
+}
+
+function onEscapeCloseModal(event) {
+  if (event.code === 'Escape') {
+    onClickCloseModal();
+  }
+  console.log(event);
+}
